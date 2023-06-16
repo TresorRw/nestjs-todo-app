2475495bd5e1f668da3fe923b17c27bfe4a730fa
TresorRw <alaintresorcyusa683@gmail.com>
2023-06-16 14:01:42 +0200

    Refactored authguard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -24,15 +24,7 @@ export class AuthGuard implements CanActivate {
     const secret = this.config.get('JWT_SECRET');
     try {
       const payload = await this.jwt.verifyAsync(token, { secret });
-      const user = await this.prisma.user.findFirst({
-        where: { id: payload.id },
-        select: { id: true, username: true, email: true },
-      });
-      if (user) {
-        request['user'] = user;
-      } else {
-        throw new Error('Invalid payload');
-      }
+      request['user'] = payload;
     } catch (error) {
       throw new NotAcceptableException('Session timed out, please login again');
     }
