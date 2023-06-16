2e178121b0f108fd2a89e7581f0722468c28f2f8
TresorRw <alaintresorcyusa683@gmail.com>
2023-06-16 11:44:16 +0200

    Ensured security concerns on headers

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -8,10 +8,15 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private config: ConfigService, private jwt: JwtService) {}
+  constructor(
+    private config: ConfigService,
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.getTokenFromHeaders(request);
@@ -19,7 +24,15 @@ export class AuthGuard implements CanActivate {
     const secret = this.config.get('JWT_SECRET');
     try {
       const payload = await this.jwt.verifyAsync(token, { secret });
-      request['user'] = payload;
+      const user = await this.prisma.user.findFirst({
+        where: { id: payload.id },
+        select: { id: true, username: true, email: true },
+      });
+      if (user) {
+        request['user'] = user;
+      } else {
+        throw new Error('Invalid payload');
+      }
     } catch (error) {
       throw new NotAcceptableException('Session timed out, please login again');
     }
