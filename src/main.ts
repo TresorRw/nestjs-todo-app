3526b3f5eb41a65b7dad231f86a6956c97af3255
TresorRw <alaintresorcyusa683@gmail.com>
2023-06-16 15:42:44 +0200

    Added CORS config

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 config();
 
 const port = process.env.PORT;
+const allowedOrigins = process.env.allowedOrigins.split(',');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({ origin: allowedOrigins, credentials: true });
   await app.listen(port, () => console.log(`🛜   http://localhost:${port}`));
 }
 bootstrap();
